3a35f5f0ac2d16fd667601f63cfa1b7754facd89
Craig Stroman <cstroman@gmail.com>
2024-03-29 13:42:34 -0400

    Updating app

--- server/app.js
+++ server/app.js
@@ -11,9 +11,29 @@ app.set('view engine', 'pug');
 
 const nodeEnv = process.env.NODE_ENV;
 
+if (nodeEnv === 'development') {
+  const webpack = require('webpack');
+  const webPackConfig = require('../webpack.config.dev.ts');
+  const webpackCompiler = webpack(webPackConfig);
+
+  app.use(
+    require('webpack-dev-middleware')(webpackCompiler, {
+      // noInfo: true,
+      publicPath: webPackConfig.output.publicPath,
+    }),
+  );
+
+  app.use(
+    require('webpack-hot-middleware')(webpackCompiler, {
+      log: false,
+      path: '/__webpack_hmr',
+    }),
+  );
+}
+
 const javascript = nodeEnv === 'development' ? '/static/js/bundle.js' : '/static/js/main.min.js';
 
-app.locals.javascript;
+app.locals.javascript = javac;
 app.locals.title = 'Node Chat';
 
 app.use('/static', express.static('public'));
