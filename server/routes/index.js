ffddf8125e6e73e0672c3916bb9dbb7442238100
Craig Stroman <cstroman@gmail.com>
2024-04-04 22:31:34 -0400

    Updating routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const passport = require('passport');
-const { main, googleLogin, login, sessionStatus } = require('../controllers/main/index');
-const { loggedIn } = require('../controllers/util/index');
+const { main } = require('../controllers/main/index');
 
 const router = express.Router();
 
 router.route('/').get(main);
-
-router.route('/login').get(login);
 
 module.exports.routes = router;
