0c22f3e67ef75f10480e3778090805cefff19f41
Craig Stroman <cstroman@gmail.com>
2024-07-14 18:16:07 -0400

    Updating the user migration

--- server/migrations/20240327030157-users.js
+++ server/migrations/20240327030157-users.js
@@ -9,7 +9,23 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER,
       },
+      firstName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
       username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
         type: Sequelize.STRING,
         allowNull: false,
       },
