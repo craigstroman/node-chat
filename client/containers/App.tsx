92dde0446e69a0fefa98a19b9a56562863df1e9a
Craig Stroman <cstroman@gmail.com>
2024-07-27 15:59:18 -0400

    Code cleanup

--- client/containers/App.tsx
+++ client/containers/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-// import { Provider } from 'react-redux';
-// import { store } from '../store';
 import Main from './main/Main';
 
 const element = document.getElementById('app');
